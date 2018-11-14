bd5b110be949d943aad9d78bc08df74ad981bfcc
Paul Soucy <paul.soucy@videray.com>
2018-11-14 17:03:21 -0500

    added astyle

--- src/MathUtils.cpp
+++ src/MathUtils.cpp
@@ -4,7 +4,8 @@
 #include <limits>
 #include <cmath>
 
-namespace baseline {
+namespace baseline
+{
 
 
 ///////////// FirstMoment ///////////////////
@@ -12,16 +13,16 @@ namespace baseline {
 template<typename T>
 FirstMoment<T>::FirstMoment()
 {
-    clear();
+  clear();
 }
 
 template<typename T>
 void FirstMoment<T>::clear()
 {
-    mN = 0;
-    m1 = std::numeric_limits<T>::quiet_NaN();
-    mDev = std::numeric_limits<T>::quiet_NaN();
-    mNDev = std::numeric_limits<T>::quiet_NaN();
+  mN = 0;
+  m1 = std::numeric_limits<T>::quiet_NaN();
+  mDev = std::numeric_limits<T>::quiet_NaN();
+  mNDev = std::numeric_limits<T>::quiet_NaN();
 
 }
 
@@ -29,26 +30,26 @@ template<typename T>
 void FirstMoment<T>::increment(T v)
 {
 
-    if (mN == 0) {
-        m1 = 0.0;
-    }
-    mN++;
-    mDev = v - m1;
-    mNDev = mDev / mN;
-    m1 += mNDev;
+  if(mN == 0) {
+    m1 = 0.0;
+  }
+  mN++;
+  mDev = v - m1;
+  mNDev = mDev / mN;
+  m1 += mNDev;
 
 }
 
 template<typename T>
 int FirstMoment<T>::getN()
 {
-    return mN;
+  return mN;
 }
 
 template<typename T>
 T FirstMoment<T>::getResult()
 {
-    return m1;
+  return m1;
 }
 
 template
@@ -63,37 +64,37 @@ class FirstMoment<double>;
 template<typename T>
 SecondMoment<T>::SecondMoment()
 {
-    clear();
+  clear();
 }
 
 template<typename T>
 void SecondMoment<T>::clear()
 {
-    FirstMoment<T>::clear();
-    m2 = std::numeric_limits<T>::quiet_NaN();
+  FirstMoment<T>::clear();
+  m2 = std::numeric_limits<T>::quiet_NaN();
 }
 
 template<typename T>
 int SecondMoment<T>::getN()
 {
-    return FirstMoment<T>::mN;
+  return FirstMoment<T>::mN;
 }
 
 template<typename T>
 void SecondMoment<T>::increment(T v)
 {
-    if (FirstMoment<T>::mN < 1) {
-        FirstMoment<T>::m1 = m2 = 0.0;
-    }
+  if(FirstMoment<T>::mN < 1) {
+    FirstMoment<T>::m1 = m2 = 0.0;
+  }
 
-    FirstMoment<T>::increment(v);
-    m2 += (FirstMoment<T>::mN - 1) * FirstMoment<T>::mDev * FirstMoment<T>::mNDev;
+  FirstMoment<T>::increment(v);
+  m2 += (FirstMoment<T>::mN - 1) * FirstMoment<T>::mDev * FirstMoment<T>::mNDev;
 }
 
 template<typename T>
 T SecondMoment<T>::getResult()
 {
-    return m2;
+  return m2;
 }
 
 template
@@ -107,50 +108,50 @@ class SecondMoment<double>;
 
 template<typename T>
 Variance<T>::Variance(bool biasCorrected)
- : mIsBiasCorrected(biasCorrected)
+  : mIsBiasCorrected(biasCorrected)
 {
 }
 
 template<typename T>
 bool Variance<T>::isBiasCorrected()
 {
-    return mIsBiasCorrected;
+  return mIsBiasCorrected;
 }
 
 template<typename T>
 void Variance<T>::increment(T v)
 {
-    moment.increment(v);
+  moment.increment(v);
 }
 
 template<typename T>
 void Variance<T>::clear()
 {
-    moment.clear();
+  moment.clear();
 }
 
 template<typename T>
 int Variance<T>::getN()
 {
-    return moment.getN();
+  return moment.getN();
 }
 
 template<typename T>
 T Variance<T>::getResult()
 {
-    const int n = moment.getN();
-    if (n == 0) {
-        return std::numeric_limits<T>::quiet_NaN();
-    } else if (n == 1) {
-        return 0.0;
+  const int n = moment.getN();
+  if(n == 0) {
+    return std::numeric_limits<T>::quiet_NaN();
+  } else if(n == 1) {
+    return 0.0;
+  } else {
+    T m2 = moment.getResult();
+    if(mIsBiasCorrected) {
+      return m2 / (n - 1);
     } else {
-        T m2 = moment.getResult();
-        if (mIsBiasCorrected) {
-            return m2 / (n - 1);
-        } else {
-            return m2 / (n);
-        }
+      return m2 / (n);
     }
+  }
 }
 
 
@@ -164,15 +165,15 @@ class Variance<double>;
 
 template<typename T>
 StandardDiviation<T>::StandardDiviation(bool biasCorrected)
- : Variance<T>(biasCorrected)
+  : Variance<T>(biasCorrected)
 {
 }
 
 template<typename T>
 T StandardDiviation<T>::getResult()
 {
-    T value = Variance<T>::getResult();
-    return std::sqrt(value);
+  T value = Variance<T>::getResult();
+  return std::sqrt(value);
 }
 
 
@@ -187,41 +188,41 @@ class StandardDiviation<double>;
 
 template<typename T>
 StorelessStats<T>::StorelessStats(bool biasCorrected)
- : mStdDiv(biasCorrected)
- {
- }
+  : mStdDiv(biasCorrected)
+{
+}
 
- template<typename T>
- void StorelessStats<T>::increment(T v)
- {
-     mMean.increment(v);
-     mStdDiv.increment(v);
- }
+template<typename T>
+void StorelessStats<T>::increment(T v)
+{
+  mMean.increment(v);
+  mStdDiv.increment(v);
+}
 
- template<typename T>
- void StorelessStats<T>::clear()
- {
-     mMean.clear();
-     mStdDiv.clear();
- }
+template<typename T>
+void StorelessStats<T>::clear()
+{
+  mMean.clear();
+  mStdDiv.clear();
+}
 
- template<typename T>
- int StorelessStats<T>::getN()
- {
-     return mMean.getN();
- }
+template<typename T>
+int StorelessStats<T>::getN()
+{
+  return mMean.getN();
+}
 
- template<typename T>
- T StorelessStats<T>::getMean()
- {
-     return mMean.getResult();
- }
+template<typename T>
+T StorelessStats<T>::getMean()
+{
+  return mMean.getResult();
+}
 
- template<typename T>
- T StorelessStats<T>::getStdDiv()
- {
-     return mStdDiv.getResult();
- }
+template<typename T>
+T StorelessStats<T>::getStdDiv()
+{
+  return mStdDiv.getResult();
+}
 
 template
 class StorelessStats<float>;
@@ -231,31 +232,31 @@ class StorelessStats<double>;
 
 template<typename T>
 LinearFunction<T> LinearFunction<T>::createFromPoints(const Vec2<T>& a, const Vec2<T>& b)
- {
-     LinearFunction<T> retval;
+{
+  LinearFunction<T> retval;
 
-     retval.mM = (a[1] - b[1]) / (a[0] - b[0]);
-     retval.mB = a[1] - a[0] * retval.mM;
+  retval.mM = (a[1] - b[1]) / (a[0] - b[0]);
+  retval.mB = a[1] - a[0] * retval.mM;
 
-     return retval;
- }
+  return retval;
+}
 
 template<typename T>
-T LinearFunction<T>::operator() (const T& input)
+T LinearFunction<T>::operator()(const T& input)
 {
-    return input * mM + mB;
+  return input * mM + mB;
 }
 
 template<typename T>
 const T& LinearFunction<T>::slope() const
 {
-    return mM;
+  return mM;
 }
 
 template<typename T>
 const T& LinearFunction<T>::offset() const
 {
-    return mB;
+  return mB;
 }
 
 template
@@ -267,13 +268,13 @@ class LinearFunction<double>;
 template<typename T>
 const T& clamp(const T& v, const T& lo, const T& hi)
 {
-    if(v < lo) {
-        return lo;
-    } else if(v > hi) {
-        return hi;
-    } else {
-        return v;
-    }
+  if(v < lo) {
+    return lo;
+  } else if(v > hi) {
+    return hi;
+  } else {
+    return v;
+  }
 }
 
 template
